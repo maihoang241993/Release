c0c142c5ff8dda5d6a9928465aeaa638ed40b80e
Hoang.mai <maihoang241993@gmail.com>
2021-06-23 15:57:43 +0700

    update port

--- Ads-FE/server.js
+++ Ads-FE/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.static(__dirname + "/dist/"));
